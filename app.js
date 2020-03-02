205780754600a9adbd78268e3033eaa8aad3809d
justinmatsnev <jmatsnev@gmail.com>
2020-03-02 16:54:10 -0500

    added translation api

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ window.onload = function() {
 function request_NewsArticles() {
   var elem = document.getElementById("sub-category");
   var category = elem.options[elem.selectedIndex].value;
-  console.log(category);
   const request = new XMLHttpRequest();
   request.open(
     "GET",
@@ -25,38 +24,7 @@ function request_NewsArticles() {
   request.send(null);
 }
 
-makeTheArticleRow = allArticles => {
-  let newsContainer = document.querySelector(".newspaper-container");
-  console.log(allArticles);
-  allArticles.forEach(singleArticle => {
-    let newsContentChildren = [];
-    let articleContent = [];
-    let articleContainer = makeAnElement("div", "class", "article-container");
-    let articleImg = makeAnElement("div", "class", "img-container");
-    articleImg.style["background-image"] = `url("${singleArticle.urlToImage}")`;
-    let newsContent = makeAnElement("div", "class", "news-content");
-    let newsAuthor = makeAnElement("h4", "class", "news-author");
-    let newsTitle = makeAnElement("p", "class", "news-title");
-    let newsDescription = makeAnElement("p", "class", "news-description");
-    let newsURL = makeAnElement("a", "href", singleArticle.url);
-    newsURL.innerHTML = singleArticle.url;
-    let newsAuthorText = document.createTextNode(singleArticle.author);
-    let newsTitleText = document.createTextNode(singleArticle.title);
-    let newsDescriptionText = document.createTextNode(
-      singleArticle.description
-    );
-    newsTitle.appendChild(newsTitleText);
-    newsAuthor.appendChild(newsAuthorText);
-    newsDescription.appendChild(newsDescriptionText);
-    newsContentChildren.push(newsAuthor, newsTitle, newsDescription, newsURL);
-    articleContent.push(articleImg, newsContent);
-    appendMultipleChild(newsContentChildren, newsContent);
-    appendMultipleChild(articleContent, articleContainer);
-    newsContainer.appendChild(articleContainer);
-  });
-};
-
-convertText = text => {
+function convertText(text) {
   const request = new XMLHttpRequest();
   let encodedText = encodeURIComponent(text.trim());
   request.open(
@@ -65,14 +33,87 @@ convertText = text => {
       encodedText,
     true
   );
+  request.setRequestHeader('X-FunTranslations-Api-Secret' ,'EDLRqx0AemHwmKnrj_WxmweF');
+
   request.onload = function() {
     if (request.status == 200) {
       data = JSON.parse(this.response);
       return data["contents"]["translated"];
     }
   };
-  request.send(null);
+  request.send();
+
 };
+
+makeTheArticleRow = allArticles => {
+  let newsContainer = document.querySelector(".newspaper-container");
+  console.log(allArticles);
+  for (i = 0; i < 1; i++) {
+    let newsContentChildren = [];
+    let articleContent = [];
+    let articleContainer = makeAnElement("div", "class", "article-container");
+    let articleImg = makeAnElement("div", "class", "img-container");
+    articleImg.style["background-image"] = `url("${allArticles[i].urlToImage}")`;
+    let newsContent = makeAnElement("div", "class", "news-content");
+    let newsAuthor = makeAnElement("h4", "class", "news-author");
+    let newsTitle = makeAnElement("p", "class", "news-title");
+    let newsDescription = makeAnElement("p", "class", "news-description");
+    let newsURL = makeAnElement("a", "href", allArticles[i].url);
+    newsURL.innerHTML = allArticles[i].url;
+    let newsAuthorText;
+    if (allArticles[i].author == 'null') {
+     newsAuthor.innerHTML = 'No Author';
+    } else {
+     newsAuthorText = document.createTextNode(allArticles[i].author);
+    }
+    let newsTitleText = document.createTextNode(allArticles[i].title);
+
+    //CHECK HERE MITCH, Trying to convert before displaying it but getting undefined
+    console.log(convertText(allArticles[i].description));
+    let newsDescriptionText = document.createTextNode(allArticles[i].description);
+    newsTitle.appendChild(newsTitleText);
+    newsAuthor.appendChild(newsAuthorText);
+    
+    newsDescription.appendChild(newsDescriptionText);
+    newsContentChildren.push(newsAuthor, newsTitle, newsDescription, newsURL);
+    articleContent.push(articleImg, newsContent);
+    appendMultipleChild(newsContentChildren, newsContent);
+    appendMultipleChild(articleContent, articleContainer);
+    newsContainer.appendChild(articleContainer);
+  }
+  // allArticles.forEach(singleArticle => {
+  //   let newsContentChildren = [];
+  //   let articleContent = [];
+  //   let articleContainer = makeAnElement("div", "class", "article-container");
+  //   let articleImg = makeAnElement("div", "class", "img-container");
+  //   articleImg.style["background-image"] = `url("${singleArticle.urlToImage}")`;
+  //   let newsContent = makeAnElement("div", "class", "news-content");
+  //   let newsAuthor = makeAnElement("h4", "class", "news-author");
+  //   let newsTitle = makeAnElement("p", "class", "news-title");
+  //   let newsDescription = makeAnElement("p", "class", "news-description");
+  //   let newsURL = makeAnElement("a", "href", singleArticle.url);
+  //   newsURL.innerHTML = singleArticle.url;
+  //   let newsAuthorText;
+  //   if (singleArticle.author == 'null') {
+  //    newsAuthor.innerHTML = 'No Author';
+  //   } else {
+  //    newsAuthorText = document.createTextNode(singleArticle.author);
+  //   }
+  //   let newsTitleText = document.createTextNode(convertText(singleArticle.title));
+  //   let newsDescriptionText = document.createTextNode(
+  //     singleArticle.description
+  //   );
+  //   newsTitle.appendChild(newsTitleText);
+  //   newsAuthor.appendChild(newsAuthorText);
+  //   newsDescription.appendChild(newsDescriptionText);
+  //   newsContentChildren.push(newsAuthor, newsTitle, newsDescription, newsURL);
+  //   articleContent.push(articleImg, newsContent);
+  //   appendMultipleChild(newsContentChildren, newsContent);
+  //   appendMultipleChild(articleContent, articleContainer);
+  //   newsContainer.appendChild(articleContainer);
+  // });
+};
+
 
 appendMultipleChild = (children, parent) => {
   children.forEach(child => {
