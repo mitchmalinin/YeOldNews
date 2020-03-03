0ebd03d1008cdbdca23d02687c5bbfd1227282a6
Mitch Malinin <mitchmalinin@gmail.com>
2020-03-03 13:55:12 -0500

    translation works

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+let allTheArticles = [];
+
 window.onload = function() {
   request_NewsArticles();
 };
@@ -11,109 +13,88 @@ function request_NewsArticles() {
     "http://newsapi.org/v2/top-headlines?country=us&category=" +
       category +
       "&apiKey=2de8bebd4cc24d51bc4bf38508bbcd7d",
-    true
+    false
   );
   request.onload = function() {
     if (request.status == 200) {
       data = JSON.parse(this.response);
       let newsContainer = document.querySelector(".newspaper-container");
       newsContainer.innerHTML = "";
-      makeTheArticleRow(data.articles);
+      let translatedArticles = [];
+      data.articles.forEach(article => {
+        //before you call this funcion take article.title and article.description and do something like "article.title" +"$"+"article.description"
+        // then translate that whole string in the function
+        let TranslateFullString = article.title + "~" + article.description;
+        console.log(TranslateFullString);
+        convertText(TranslateFullString);
+        function convertText(text) {
+          const request = new XMLHttpRequest();
+          let encodedText = encodeURIComponent(text.trim());
+          request.open(
+            "GET",
+            "https://api.funtranslations.com/translate/shakespeare.json?text=" +
+              encodedText,
+            true
+          );
+          request.setRequestHeader(
+            "X-FunTranslations-Api-Secret",
+            "EDLRqx0AemHwmKnrj_WxmweF"
+          );
+
+          request.onload = function() {
+            if (request.status == 200) {
+              translatedDescription = JSON.parse(this.response);
+              //here you would do let splitArr = translatedDescription.contents.translated.split('$')
+              //this should give you an array with 2 index, splitArr[0] should be title and splitArr[1] should be description
+              // then do article.title = splitArr[0].join('') and article.description = splitArr[1].join('')
+
+              let splitArr = translatedDescription.contents.translated.split(
+                "~"
+              );
+              article.title = splitArr[0];
+              article.description = splitArr[1];
+              makeTheArticleRow(article);
+            }
+          };
+          request.send();
+        }
+      });
     }
   };
   request.send(null);
 }
 
-function convertText(text) {
-  const request = new XMLHttpRequest();
-  let encodedText = encodeURIComponent(text.trim());
-  request.open(
-    "GET",
-    "https://api.funtranslations.com/translate/shakespeare.json?text=" +
-      encodedText,
-    true
-  );
-  request.setRequestHeader('X-FunTranslations-Api-Secret' ,'EDLRqx0AemHwmKnrj_WxmweF');
-
-  request.onload = function() {
-    if (request.status == 200) {
-      data = JSON.parse(this.response);
-      return data["contents"]["translated"];
-    }
-  };
-  request.send();
-
-};
-
-makeTheArticleRow = allArticles => {
+makeTheArticleRow = theArticle => {
   let newsContainer = document.querySelector(".newspaper-container");
-  console.log(allArticles);
-  for (i = 0; i < 1; i++) {
-    let newsContentChildren = [];
-    let articleContent = [];
-    let articleContainer = makeAnElement("div", "class", "article-container");
-    let articleImg = makeAnElement("div", "class", "img-container");
-    articleImg.style["background-image"] = `url("${allArticles[i].urlToImage}")`;
-    let newsContent = makeAnElement("div", "class", "news-content");
-    let newsAuthor = makeAnElement("h4", "class", "news-author");
-    let newsTitle = makeAnElement("p", "class", "news-title");
-    let newsDescription = makeAnElement("p", "class", "news-description");
-    let newsURL = makeAnElement("a", "href", allArticles[i].url);
-    newsURL.innerHTML = allArticles[i].url;
-    let newsAuthorText;
-    if (allArticles[i].author == 'null') {
-     newsAuthor.innerHTML = 'No Author';
-    } else {
-     newsAuthorText = document.createTextNode(allArticles[i].author);
-    }
-    let newsTitleText = document.createTextNode(allArticles[i].title);
-
-    //CHECK HERE MITCH, Trying to convert before displaying it but getting undefined
-    console.log(convertText(allArticles[i].description));
-    let newsDescriptionText = document.createTextNode(allArticles[i].description);
-    newsTitle.appendChild(newsTitleText);
-    newsAuthor.appendChild(newsAuthorText);
-    
-    newsDescription.appendChild(newsDescriptionText);
-    newsContentChildren.push(newsAuthor, newsTitle, newsDescription, newsURL);
-    articleContent.push(articleImg, newsContent);
-    appendMultipleChild(newsContentChildren, newsContent);
-    appendMultipleChild(articleContent, articleContainer);
-    newsContainer.appendChild(articleContainer);
+  console.log("all the articles", theArticle);
+  let newsContentChildren = [];
+  let articleContent = [];
+  let articleContainer = makeAnElement("div", "class", "article-container");
+  let articleImg = makeAnElement("div", "class", "img-container");
+  articleImg.style["background-image"] = `url("${theArticle.urlToImage}")`;
+  let newsContent = makeAnElement("div", "class", "news-content");
+  let newsAuthor = makeAnElement("h4", "class", "news-author");
+  let newsTitle = makeAnElement("p", "class", "news-title");
+  let newsDescription = makeAnElement("p", "class", "news-description");
+  let newsURL = makeAnElement("a", "href", theArticle.url);
+  newsURL.innerHTML = theArticle.url;
+  let newsAuthorText;
+  if (theArticle.author == "null") {
+    newsAuthor.innerHTML = "No Author";
+  } else {
+    newsAuthorText = document.createTextNode(theArticle.author);
   }
-  // allArticles.forEach(singleArticle => {
-  //   let newsContentChildren = [];
-  //   let articleContent = [];
-  //   let articleContainer = makeAnElement("div", "class", "article-container");
-  //   let articleImg = makeAnElement("div", "class", "img-container");
-  //   articleImg.style["background-image"] = `url("${singleArticle.urlToImage}")`;
-  //   let newsContent = makeAnElement("div", "class", "news-content");
-  //   let newsAuthor = makeAnElement("h4", "class", "news-author");
-  //   let newsTitle = makeAnElement("p", "class", "news-title");
-  //   let newsDescription = makeAnElement("p", "class", "news-description");
-  //   let newsURL = makeAnElement("a", "href", singleArticle.url);
-  //   newsURL.innerHTML = singleArticle.url;
-  //   let newsAuthorText;
-  //   if (singleArticle.author == 'null') {
-  //    newsAuthor.innerHTML = 'No Author';
-  //   } else {
-  //    newsAuthorText = document.createTextNode(singleArticle.author);
-  //   }
-  //   let newsTitleText = document.createTextNode(convertText(singleArticle.title));
-  //   let newsDescriptionText = document.createTextNode(
-  //     singleArticle.description
-  //   );
-  //   newsTitle.appendChild(newsTitleText);
-  //   newsAuthor.appendChild(newsAuthorText);
-  //   newsDescription.appendChild(newsDescriptionText);
-  //   newsContentChildren.push(newsAuthor, newsTitle, newsDescription, newsURL);
-  //   articleContent.push(articleImg, newsContent);
-  //   appendMultipleChild(newsContentChildren, newsContent);
-  //   appendMultipleChild(articleContent, articleContainer);
-  //   newsContainer.appendChild(articleContainer);
-  // });
+  let newsTitleText = document.createTextNode(theArticle.title);
+  let newsDescriptionText = document.createTextNode(theArticle.description);
+  newsTitle.appendChild(newsTitleText);
+  newsAuthor.appendChild(newsAuthorText);
+  newsDescription.appendChild(newsDescriptionText);
+  newsContentChildren.push(newsAuthor, newsTitle, newsDescription, newsURL);
+  articleContent.push(articleImg, newsContent);
+  appendMultipleChild(newsContentChildren, newsContent);
+  appendMultipleChild(articleContent, articleContainer);
+  newsContainer.appendChild(articleContainer);
 };
-
 
 appendMultipleChild = (children, parent) => {
   children.forEach(child => {
